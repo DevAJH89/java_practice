6f0977df5d416bdb6dfd1595227ddb4d1b1c61dc
DevAJH89 <DevAJH89@gmail.com>
2026-03-13 17:33:51 +0900

    feat : 초기화

---
+++ 00_MyFirstJavaProject/src/com/kh/second/PrintController.java
@@ -0,0 +1,8 @@
+package com.kh.second;
+
+public class PrintController {
+	
+	public void printMyName(){
+		System.out.print("안녕 내 이름은 안지호야");
+	}
+}
---
+++ 01_Variable/src/com/kh/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.run;
+
+//import 다른 패키지에 있는 소스코드를 사용하고 싶을때 사용
+import com.kh.variable.Variable; 
+
+public class Run {
+	//흥미로운걸
+	public static void main(String[] args) {
+		Variable v = new Variable();
+		
+		//v.findLunchMenu();
+		v.declareVariable();
+	}
+}
